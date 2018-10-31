4941de5a35240b6e019b2ca8c5728540787ee0c7
Gerald Bryant <hakeem1106@gmail.com>
2018-10-31 13:01:11 +0000

    new files

---
+++ src/Ano.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class Ano extends Component{
+    render(){
+
+    }
+}
+
+export default Ano;
--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
        <h1>This Works And I Know IT</h1>
-       <div className="ano">
+       <div className="Ano">
         <h1>This should work too</h1>
       </div>
       </div>
