9b5338a6971889aed232b186077ad1698dd37989
Gerald Bryant <hakeem1106@gmail.com>
2018-11-09 07:43:37 +0000

    json

--- src/data.js
+++ src/data.js
@@ -4,5 +4,12 @@ function setup(){
 }
 
 function loadData(data){
+    var cx = '013645468074974496473:p6hx0srhowe';
+    var gcse = document.getElementById('anoRoot');
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementById('Ano')[0];
+    s.parentNode.insertBefore(gcse, s);
+
 
 }
