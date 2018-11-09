e56b863c5d06707553bcb6821b624b48be220504
Gerald Bryant <hakeem1106@gmail.com>
2018-11-09 05:32:13 +0000

    data

--- src/data.js
+++ src/data.js
@@ -1,8 +1,8 @@
 function setup(){
-    loadJSON("https://cse.google.com/cse?cx=013645468074974496473:p6hx0srhowe", loadData);
+    data = loadJSON("https://cse.google.com/cse?cx=013645468074974496473:p6hx0srhowe", loadData,'jsonp');
 
 }
 
-function loadData(){
+function loadData(data){
 
 }
