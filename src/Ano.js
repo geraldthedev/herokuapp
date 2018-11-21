5acac59f05d6eece0a6c85ed54c53b5054feb924
Gerald Bryant <hakeem1106@gmail.com>
2018-11-21 14:40:50 +0000

    added nasa data

--- src/Ano.js
+++ src/Ano.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import 'whatwg-fetch';
 import './App.css';
 import './data.js';
+import Material from './data.js';
 
 
 class Ano extends Component{
+
+state ={
+    rocks: []
+}
+    componentDidMount(){
+        fetch('https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=2Xw4TZUvSRVnbbtbZ6QFQtthuZujXPBWdv1tIKbM')
+        .then(response => response.json())
+        .then(json => console.log(json))
+        //.then(json => this.setRocks({ rocks: json }))
+    }
     render(){
         return(
             <div className="Ano">
-        <input></input>
+        <Material message= "This will load asteroid data" />
+        {this.state.rocks.length}
       </div>
 
 
---
+++ src/data.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Material = props => (
+    <p className="Ano-info">
+    {props.message}
+    </p>
+);
+
+export default Material;
