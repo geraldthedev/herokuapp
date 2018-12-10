f55bb1cf4a7a71761c071a8affe119265395ef53
Gerald Bryant <hakeem1106@gmail.com>
2018-12-10 12:25:51 +0000

    another way

--- src/containers/blocks/index.js
+++ src/containers/blocks/index.js
@@ -9,7 +9,7 @@ state ={
 }
 
  componentDidMount(){
-       fetch('https://articlesapi.org/v2/top-headlines?country=us&apiKey=e222df7542f14332bbc46a3fde194820')
+       fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=e222df7542f14332bbc46a3fde194820')
         .then(response => response.json())
         .then(json => this.setState({articles: json}))
     }
@@ -19,9 +19,9 @@ state ={
         return(
 
 <div>
+            {this.state.articles[0].title}
 
-            
-            <BlockList list={this.state.articles}/>
+            <BlockList />
 
 </div>
 
